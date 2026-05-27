e7c3dc5be7d0caf8b7634a1ffcd04d890e27a539
KRISHNAVENI C <159801055+kveni1105@users.noreply.github.com>
2026-05-27 09:20:54 +0530

    Time: 0 ms (100%), Space: 45.5 MB (18.55%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -4,15 +4,14 @@ class Solution {
         int prod=1;
         for(int i=0; i<n; i++)
         {
-           if(nums[i]==0)
-           {
-            return 0;
-           }
-           if(nums[i]<0)
-           {
-            prod *=-1;
-           }
-
+            if(nums[i]==0)
+            {
+                return 0;
+            }
+            if(nums[i]<0)
+            {
+                prod *=-1;
+            }
         }
         return prod;
     }
